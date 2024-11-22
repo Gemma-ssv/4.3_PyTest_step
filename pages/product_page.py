0a96056d2ed54ac02e37d275281128198acb2dd0
Gemma-ssv <eldarbolshov@gmail.com>
2024-11-22 09:19:12 +0300

    change product_page

---
+++ pages/product_page.py
@@ -0,0 +1,42 @@
+import time
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+
+class ProductPage(BasePage): 
+    def go_to_caart_page(self):
+        time.sleep(3)
+        add_cart = self.browser.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/form/button")
+        name_product = self.browser.find_element(By.CSS_SELECTOR, "html.no-js body#default.default div.container-fluid.page div.page_inner div.content div#content_inner article.product_page div.row div.col-sm-6.product_main h1").text
+        price_product = self.browser.find_element(By.CLASS_NAME, "price_color").text
+        add_cart.click()
+        self.quiz()
+        name_product2 = self.browser.find_element(By.CSS_SELECTOR,"#messages > div:nth-child(1) > div > strong").text
+        
+        cart = self.browser.find_element(By.CSS_SELECTOR,"#messages > div:nth-child(2) > div > strong").text
+        
+        price_product2 = self.browser.find_element(By.CSS_SELECTOR,"#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong").text
+        
+        assert name_product == name_product2
+
+        assert cart == 'Deferred benefit offer'
+        
+        assert price_product == price_product2
+    
+    def go_to_caart_page_2(self):
+        time.sleep(3)
+        add_cart = self.browser.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/form/button")
+        add_cart.click()
+        self.quiz()    
+        assert self.is_not_element_present(By.CSS_SELECTOR,"#messages > div:nth-child(2) > div > strong")
+    
+    def go_to_caart_page_3(self):
+        time.sleep(3)
+        assert self.is_not_element_present(By.CSS_SELECTOR,"#messages > div:nth-child(2) > div > strong")
+    
+    def go_to_caart_page_4(self):
+        time.sleep(3)
+        add_cart = self.browser.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/form/button")
+        add_cart.click()
+        self.quiz() 
+        assert self.is_disappeared(By.CSS_SELECTOR,"#messages > div:nth-child(2) > div > strong")
+        
